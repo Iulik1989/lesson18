2bc480ca80ae168ce15c7df80214cc55599f6252
Iulik <inicula1989@gmail.com>
2024-03-18 21:01:44 +0200

    first

---
+++ index.js
@@ -0,0 +1,64 @@
+// const arr = [1,2,3]
+
+// const obj1 = {
+//     key: '1',
+//     key1: '1',
+//     key2: '1',
+// }
+
+
+// const arr1 = arr.map((item) => {
+//     return item + 1;
+// }).map((item) => {
+//     return item * 2
+// }).join('').split('')
+
+
+
+
+// console.log(Object.keys(obj1))
+
+
+const arr = [13, 23, 32, 4333, 544, 65, 6667]
+const strArr = ['Vasile', 'Alexandru', 'Boris', 'Constantin']
+
+const arrObj = [
+    {
+        nume: 'Vasile0'
+    },
+    {
+        nume: 'Vasile100'
+    },
+    {
+        nume: 'Vasile2'
+    },
+]
+
+// const someValue = arr.some((item, index) => {
+//     console.log(index)
+//     return item > 3;
+// })
+
+// const strArrayValue = strArr.some((item) => item.length > 4);
+
+// const everyValue = strArr.every((item, index) => {
+//     console.log(index)
+//     return item.length > 3; 
+// })
+
+// const findItem = arr.find((item) => {
+//     const convertedItem = item.toString()
+//     return convertedItem.includes('5')
+// })
+
+// const indexOfItem = arr.findIndex((item) => {
+//     return item > 13;
+
+// })
+
+// const sorted = arrObj.sort((a, b) => {
+//     return a.nume < b.nume ? -1 : 1;
+
+// })
+
+// console.log(sorted)
